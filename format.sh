aa13a2c87fa0d0d2e3bb13c7d6723a36dba0e468
Shiv Upadhyay <shivnupadhyay@gmail.com>
2019-12-18 02:26:38 -0500

    Using doctest discover tests to find tests

--- format.sh
+++ format.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
+find . -name 'CMakeLists.txt' -exec cmake-format -i {} \;
--- tests/basis_test.cpp
+++ tests/basis_test.cpp
@@ -1,4 +1,4 @@
+#include <basis/basis.hpp>
 #include <doctest/doctest.h>
-#include <basis.h>
 
 TEST_CASE("BASIS TESTS") { CHECK(0 == 0); }
