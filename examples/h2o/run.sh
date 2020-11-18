34262775975895c94122a795d3be13e1227445d3
Shiv Upadhyay <shivnupadhyay@gmail.com>
2020-11-18 11:34:22 -0500

    updating run scripts

--- examples/h2o/run.sh
+++ examples/h2o/run.sh
@@ -4,16 +4,16 @@ export OMP_NUM_THREADS=1
 
 ../../build/bin/polyquant -i h2o.json
 ~/Documents/qmcpack/build/bin/convert4qmc -orbitals electron.h5
-sed -i 's/blocks">20/blocks">1000/' electron.qmc.in-wfnoj.xml
+sed -i 's/blocks">20/blocks">100/' electron.qmc.in-wfnoj.xml
 sed -i 's/steps">50/steps">3000/' electron.qmc.in-wfnoj.xml
-#sed -i 's/usedrift">no/usedrift">yes/' electron.qmc.in-wfnoj.xml
+sed -i 's/usedrift">no/usedrift">yes/' electron.qmc.in-wfnoj.xml
 
 cd pyscf
 python h2o.py
 ~/Documents/qmcpack/build/bin/convert4qmc -orbitals Default.h5
-sed -i 's/blocks">20/blocks">1000/' Default.qmc.in-wfnoj.xml
+sed -i 's/blocks">20/blocks">100/' Default.qmc.in-wfnoj.xml
 sed -i 's/steps">50/steps">3000/' Default.qmc.in-wfnoj.xml
-#sed -i 's/usedrift">no/usedrift">yes/' Default.qmc.in-wfnoj.xml
+sed -i 's/usedrift">no/usedrift">yes/' Default.qmc.in-wfnoj.xml
 cd ..
 
 h5dump pyscf/Default.h5 > guide.txt; h5dump electron.h5 > output.txt
