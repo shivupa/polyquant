3dbaba63c63a4533cda2ec2ee130cf4943cb6229
Shiv Upadhyay <shivnupadhyay@gmail.com>
2021-04-06 15:08:03 -0400

    construct cache

--- src/integral/integral.cpp
+++ src/integral/integral.cpp
@@ -9,6 +9,7 @@ POLYQUANT_INTEGRAL::POLYQUANT_INTEGRAL(const POLYQUANT_INPUT &input,
   POLYQUANT_TIMER timer(function);
   Polyquant_cout("INTEGRAL");
   this->setup_integral(input, basis, molecule);
+  this->construct_cache();
 }
 
 void POLYQUANT_INTEGRAL::construct_cache(size_t size_in_gb){
