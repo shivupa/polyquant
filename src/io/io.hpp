3ce1634bd498914fa997b6b00cad7bde7f5b5b99
Shiv Upadhyay <shivnupadhyay@gmail.com>
2021-07-11 22:52:38 -0400

    separate out hdf5 utilities

--- src/io/io.hpp
+++ src/io/io.hpp
@@ -90,27 +90,6 @@ void Polyquant_dump_vec(const Eigen::Matrix<T, Eigen::Dynamic, 1> &vec,
   }
 }
 
-void Polyquant_dump_post_mf_to_hdf5_for_QMCPACK(
-    const std::string &filename,
-    std::vector<std::vector<std::vector<std::vector<uint64_t>>>> dets,
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> C, int N_dets,
-    int N_states, int N_mo);
-
-void Polyquant_dump_mf_to_hdf5_for_QMCPACK(
-    const std::string &filename, bool pbc, bool complex_vals, bool ecp,
-    bool restricted, int num_ao, int num_mo, bool bohr_unit, int num_part_alpha,
-    int num_part_beta, int num_part_total, int multiplicity, int num_atom,
-    int num_species, std::vector<std::string> quantum_part_names,
-    std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, 1>>> E_orb,
-    std::vector<
-        std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>>
-        mo_coeff,
-    std::vector<int> atomic_species_ids, std::vector<int> atomic_number,
-    std::vector<int> atomic_charge, std::vector<int> core_elec,
-    std::vector<std::string> atomic_names,
-    std::vector<std::vector<double>> atomic_centers,
-    std::vector<std::vector<libint2::Shell>> unique_shells);
-
 /**
  * @brief A helper function to dump a dense vector object to file.
  *
