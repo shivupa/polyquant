9a3c0c5b88a2d205535323460e74e80c743836cc
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-01-02 14:08:57 -0500

    modify printing

--- src/io/davidson_logging.hpp
+++ src/io/davidson_logging.hpp
@@ -17,7 +17,7 @@ template <typename Scalar, typename Vector> class DavidsonDerivedLogger : public
 public:
   DavidsonDerivedLogger(){};
   inline void iteration_log(const Spectra::IterationData<Scalar, Vector> &data) override final {
-    std::cout << "--------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "                                                                                            " << std::endl;
     std::cout << "    Iteration                       :   " << data.iteration << std::endl;
     std::cout << "    Number of converged eigenvalues :   " << data.number_of_converged << std::endl;
     std::cout << "    Size of subspace                :   " << data.subspace_size << std::endl;
@@ -27,7 +27,7 @@ public:
     for (int i = 0; i < data.current_eigenvalues.size(); i++) {
       std::cout << "       " << std::setw(20) << data.current_eigenvalues[i] << std::setw(20) << data.current_eig_converged[i] << std::setw(20) << data.residues[i] << std::endl;
     }
-    std::cout << "--------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "                                                                                            " << std::endl;
   };
 };
 } // namespace polyquant
