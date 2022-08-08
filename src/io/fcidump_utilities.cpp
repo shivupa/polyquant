46a715b5d826647eeabe7a07165d49acd2d6b28a
amandadumi <amanda.e.dumi@gmail.com>
2022-08-07 21:10:19 -0400

    first pass integral functions

--- src/io/fcidump_utilities.cpp
+++ src/io/fcidump_utilities.cpp
@@ -18,8 +18,7 @@ void POLYQUANT_FCIDUMP::create_file(const std::string &fname) {
   void dump(int num_mo, int num_part_total,int ms2, bool restricted, std::vector<int> MO_symmetry_labels, int isym, std::string point_group, std::vector<std::vector<Eigen::Matrix<double,Eigen::dynamic, Eigen::Dynamic>>> mo_one_body_ints, std::vector<std::vector<std::vector<std::vector<Eigen::Matrix<double,Eigen::Dynamic, Eigen::Dynamic>>>>>mo_two_body_ints, int quantum_part_a_idx,int quantum_part_b_idx ){
     quantum_part_a_index = quantum_part_a_idx;
     quantum_part_a_index = quantum_part_a_idx;
-   // if this is an FCIDUMP for multiple types, 
-   int spin_types=1;//default case, restricted
+    // if this is an FCIDUMP for the same types, 
     if quantum_part_a_index == quantum_part_b_idx{
       this->dump_header(num_mo, num_part_tot, ms2,restricted, MO_symmetry_labels, isym, point_group);
     }
@@ -36,7 +35,7 @@ void POLYQUANT_FCIDUMP::create_file(const std::string &fname) {
    }
 
    // variables
-  void dump_header(int num_mo, int num_part_tot, int ms2, bool restricted, std::vector<int> MO_symmetry_labels, int isym, std::string point_group){
+  void dump_header(int num_mo, int num_part_tot, int ms2, bool restricted, std::vector<int> mo_symmetry_labels, int isym, std::string point_group){
   /*
    * will look something like:
    *  &FCI NORB=  4,NELEC=  3,MS2= 1,
@@ -46,25 +45,48 @@ void POLYQUANT_FCIDUMP::create_file(const std::string &fname) {
   */
   this->fcidump_file << std::setw(10) << "&FCI  NORB= " << std::setw(5) << num_mo << std::setw(9) <<", NELEC= " << std::setw(4) << num_part_tot << std::setw(5) << "M2S= " << std::setw(4) <<ms2 << std::endl;
   this->fcidump_file << std::setw(10) << "ORBSYM= "  
-   for (int i=0;i, MO_symmetry_labels.siz()){
-    this->fcidump_file  << std::setw(3)<< MO_symmetry_labels.at(i);
+   for (int i=0;i, mo_symmetry_labels.siz()){
+    this->fcidump_file  << std::setw(3)<< mo_symmetry_labels.at(i);
    }
     this->fcidump_file << ',' <<std::endl;
-  this->fcidump_file << std::setw(10) << "ISYM= "  << MO_symmetry_labels<< std::endl;
+  this->fcidump_file << std::setw(10) << "ISYM= "  << mo_symmetry_labels<< std::endl;
   this->fcidump_file << std::setw(10) << "IUHF= "  << !restricted << std::endl;
-  this->fcidump_file << std::setw(10) << "PNT_GRP= "  << MO_symmetry_labels<< std::endl;
-    this->fcidump_file << std::setw(3) << "*/"  << MO_symmetry_labels<< std::endl;}
+  this->fcidump_file << std::setw(10) << "PNT_GRP= "  << std::setw(4)<< point_group << std::endl;
+    this->fcidump_file << std::setw(3) << "*/"  << mo_symmetry_labels<< std::endl;}
 
   
   void dump_one_body_ints(std::vector<std::vector<double,Eigen::Dynamic,Eigen::Dynamic>>mo_one_body_ints){
-	   std::string placeholder
+	   //if restricted loop over alpha and beta
+    int rows = mo_one_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b].rows()
+    int cols = mo_one_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b].cols()
+    for (int spin_a=0; spin_a< spin_types ; spin_a ++)
+      for (int spin_b=0; spin_b< spin_types ; spin_b ++)
+        for (int i=0; i < rows ; i ++)
+          for (int a=0; a < cols ; a ++){
+              ints = mo_two_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b]
+              this->fcidump_file << std::setw(10) << mo_one_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b](i,a)<< std::setw(3)<< i+1 <<std::setw(3)<< a+1 << std::endl;
+            }
+  }
   }
 
 
   void dump_two_body_ints(std::vector<std::vector,std::vector<std::vector<double,Eigen::Dynamic,Eigen::Dynamic>>>> mo_two_body_ints, int num_mo,num_part_tot){
-	   std::string placeholder
+	   //if restricted loop over alpha and beta
+     int spin_types=1;
+     if !restricted{
+      spin_types = 2;
+     }
+    for (int spin_a=0; spin_a< spin_types ; spin_a ++)
+      for (int spin_b=0; spin_b< spin_types ; spin_b ++)
+        for (int i=0; i< rows ; i ++)
+          for (int j=0; j< rows ; j ++)
+           for (int a=0; a< cols ; a ++)
+            for (int b=0; b< cols ; b ++){
+              ints = mo_two_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b]
+              this->fcidump_file << std::setw(10) << mo_two_body_ints[quantum_part_a_index][spin_a][quantum_part_b_idx][spin_b](i,j,a,b)<< std::setw(3)<< i+1 <<std::setw(3)<< j+1 <<std::setw(3)<< a+1 <<std::setw(3)<< b+1 << std::endl;
+            }
   }
 
    void dump_other_vals(){
-	   std::string placeholder
+	   std::string placeholder;
    }
