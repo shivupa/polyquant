a1d237705d1e5dfe59c3f5369d61cceb21e8289a
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-08-29 16:48:10 -0400

    Better NO  building
    new:
    polyquant::POLYQUANT_EPCI::calculate_NOs()
    0d:0h:0m:0s:2ms:933us:158ns
    
    old:
    polyquant::POLYQUANT_EPCI::calculate_NOs()
    0d:0h:0m:0s:153ms:422us:759ns
    
    Tiny example h2o/sto-3g cisd but it must be faster.

--- src/ci/determinant_set.hpp
+++ src/ci/determinant_set.hpp
@@ -1795,63 +1795,54 @@ void POLYQUANT_DETSET<T>::create_1rdm(const int state_idx, const int quantum_par
       auto ishift = 0;
       auto Di = this->get_det(quantum_part_idx, quantum_part_spin_idx, idx_idet);
       // diagonal
-      // for (auto int_idx = 0; int_idx < Di.size(); int_idx++) {
-      //   buffer = Di[int_idx];
-      //   while (buffer != 0) {
-      //     auto position = std::countr_zero(buffer);
-      //     auto orb_idx = ((Di.size() - int_idx - 1) * 64) + position;
-      //     MO_rdm1(orb_idx, orb_idx) += C(i_det, state_idx) * C(i_det, state_idx);
-      //     buffer &= buffer - 1UL;
-      //   }
-      // }
-      // off diagonal singles contributions
       std::vector<int> occ, virt;
       this->get_occ_virt(quantum_part_idx, Di, occ, virt);
       for (auto orb_idx : occ) {
         MO_rdm1_thread_contributions[thread_id](orb_idx, orb_idx) += C(i_det, state_idx) * C(i_det, state_idx);
       }
-      for (auto j_det = 0; j_det < i_det; j_det++) {
-        auto j_unfold = det_idx_unfold(j_det);
-        auto idx_jdet = j_unfold[2 * quantum_part_idx + quantum_part_spin_idx];
+      // off diagonal singles contributions
+      for (auto idx_jdet : unique_singles[quantum_part_idx][quantum_part_spin_idx][idx_idet]) {
         auto Dj = this->get_det(quantum_part_idx, quantum_part_spin_idx, idx_jdet);
-        auto num_excitation = single_spin_num_excitation(Di, Dj);
-        // confirm that the excitation level is 1 for the part/spin of interest
-        if (num_excitation != 1) {
-          continue;
+        std::vector<int> j_unfold = i_unfold;
+        j_unfold[2 * quantum_part_idx + quantum_part_spin_idx] = idx_jdet;
+        if (this->dets.find(j_unfold) != this->dets.end()) {
+          auto j_det = this->dets.find(j_unfold)->second;
+          if (i_det < j_det) {
+            continue;
+          }
+          // calculate over all excitation level
+          auto num_ex = 0;
+          auto num_parts = this->input_integral.input_molecule.quantum_particles.size();
+          for (int excitation_quantum_part_idx = 0; excitation_quantum_part_idx < num_parts; excitation_quantum_part_idx++) {
+            auto idx_idet_spin0 = i_unfold[2 * excitation_quantum_part_idx + (quantum_part_spin_idx)];
+            auto idx_idet_spin1 = i_unfold[2 * excitation_quantum_part_idx + (1 - quantum_part_spin_idx)];
+            auto idx_jdet_spin0 = j_unfold[2 * excitation_quantum_part_idx + (quantum_part_spin_idx)];
+            auto idx_jdet_spin1 = j_unfold[2 * excitation_quantum_part_idx + (1 - quantum_part_spin_idx)];
+            auto Di_spin0 = this->get_det(excitation_quantum_part_idx, quantum_part_spin_idx, idx_idet_spin0);
+            auto Dj_spin0 = this->get_det(excitation_quantum_part_idx, quantum_part_spin_idx, idx_jdet_spin0);
+            auto Di_spin1 = this->get_det(excitation_quantum_part_idx, 1 - quantum_part_spin_idx, idx_idet_spin1);
+            auto Dj_spin1 = this->get_det(excitation_quantum_part_idx, 1 - quantum_part_spin_idx, idx_jdet_spin1);
+            auto det_i = std::make_pair(Di_spin0, Di_spin1);
+            auto det_j = std::make_pair(Dj_spin0, Dj_spin1);
+            num_ex += this->num_excitation(det_i, det_j);
+          }
+          if (num_ex != 1) {
+            continue;
+          }
+          std::vector<int> holes, parts;
+          double phase = 1.0;
+          holes.clear();
+          parts.clear();
+          get_holes(Di, Dj, holes);
+          get_parts(Di, Dj, parts);
+          phase = get_phase(Di, Dj, holes, parts);
+          auto contribution = phase * C(i_det, state_idx) * C(j_det, state_idx);
+          // maybe contribution *= 2;
+          auto k = holes[0];
+          auto l = parts[0];
+          MO_rdm1_thread_contributions[thread_id](k, l) += contribution;
+          MO_rdm1_thread_contributions[thread_id](l, k) += contribution;
         }
-
-        // calculate over all excitation level
-        num_excitation = 0;
-        auto num_parts = this->input_integral.input_molecule.quantum_particles.size();
-        for (int excitation_quantum_part_idx = 0; excitation_quantum_part_idx < num_parts; excitation_quantum_part_idx++) {
-          auto idx_idet_spin0 = i_unfold[2 * quantum_part_idx + (quantum_part_spin_idx)];
-          auto idx_idet_spin1 = i_unfold[2 * quantum_part_idx + (1 - quantum_part_spin_idx)];
-          auto idx_jdet_spin0 = j_unfold[2 * quantum_part_idx + (quantum_part_spin_idx)];
-          auto idx_jdet_spin1 = j_unfold[2 * quantum_part_idx + (1 - quantum_part_spin_idx)];
-          auto Di_spin0 = this->get_det(quantum_part_idx, quantum_part_spin_idx, idx_idet_spin0);
-          auto Dj_spin0 = this->get_det(quantum_part_idx, quantum_part_spin_idx, idx_jdet_spin0);
-          auto Di_spin1 = this->get_det(quantum_part_idx, 1 - quantum_part_spin_idx, idx_idet_spin1);
-          auto Dj_spin1 = this->get_det(quantum_part_idx, 1 - quantum_part_spin_idx, idx_jdet_spin1);
-          auto det_i = std::make_pair(Di_spin0, Di_spin1);
-          auto det_j = std::make_pair(Dj_spin0, Dj_spin1);
-          num_excitation += this->num_excitation(det_i, det_j);
-        }
-        if (num_excitation != 1) {
-          continue;
-        }
-        std::vector<int> holes, parts;
-        double phase = 1.0;
-        holes.clear();
-        parts.clear();
-        get_holes(Di, Dj, holes);
-        get_parts(Di, Dj, parts);
-        phase = get_phase(Di, Dj, holes, parts);
-        auto contribution = phase * C(i_det, state_idx) * C(j_det, state_idx);
-        // maybe contribution *= 2;
-        auto k = holes[0];
-        auto l = parts[0];
-        MO_rdm1_thread_contributions[thread_id](k, l) += contribution;
-        MO_rdm1_thread_contributions[thread_id](l, k) += contribution;
       }
     }
   }
