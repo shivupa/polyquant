067c5618a9df19b9537baa0cefab2b5cbe3868f5
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-09-09 02:21:05 -0400

    Update examples

--- examples/h2o_ci/temp.py
+++ examples/h2o_ci/temp.py
@@ -12,27 +12,28 @@ mf.analyze()
 cisolver = pyscf.fci.FCI(mf)
 print('E(FCI) = %.12f' % cisolver.kernel()[0])
 
-from pyscf.tools import fcidump
-fcidump.from_scf(mf, 'fcidump.example1', tol=0)
-exit()
-mycc = mf.CISD().run()
-print('RCISD correlation energy', mycc.e_corr)
 
-#mf = mol.UHF().run()
-#mycc = mf.CISD().run()
-#print('UCISD correlation energy', mycc.e_corr)
-
-
-t = mol.intor('int1e_kin')
-v = mol.intor('int1e_nuc')
-h = t + v
-
-h_mo = mf.mo_coeff.T @ h @ mf.mo_coeff
-s_mo = mf.mo_coeff.T @ mol.intor('int1e_ovlp') @ mf.mo_coeff
-
-
-eri_4fold = pyscf.ao2mo.kernel(mol, mf.mo_coeff)
-
-
-from utils import cisd
-cisd(mol, printroots=2)
+# from pyscf.tools import fcidump
+# fcidump.from_scf(mf, 'fcidump.example1', tol=0)
+# exit()
+# mycc = mf.CISD().run()
+# print('RCISD correlation energy', mycc.e_corr)
+# 
+# #mf = mol.UHF().run()
+# #mycc = mf.CISD().run()
+# #print('UCISD correlation energy', mycc.e_corr)
+# 
+# 
+# t = mol.intor('int1e_kin')
+# v = mol.intor('int1e_nuc')
+# h = t + v
+# 
+# h_mo = mf.mo_coeff.T @ h @ mf.mo_coeff
+# s_mo = mf.mo_coeff.T @ mol.intor('int1e_ovlp') @ mf.mo_coeff
+# 
+# 
+# eri_4fold = pyscf.ao2mo.kernel(mol, mf.mo_coeff)
+# 
+# 
+# from utils import cisd
+# cisd(mol, printroots=2)
