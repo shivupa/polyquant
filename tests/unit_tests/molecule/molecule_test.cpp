43caf01bfc12b9c10e32ae8280929f433a8ba58f
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-11-03 13:22:08 -0400

    test fixes

--- tests/unit_tests/molecule/molecule_test.cpp
+++ tests/unit_tests/molecule/molecule_test.cpp
@@ -10,22 +10,20 @@ TEST_SUITE("MOLECULE") {
     POLYQUANT_INPUT test_inp("../../tests/data/h2o_sto3glibrary/h2o.json");
     POLYQUANT_MOLECULE test_mol;
     test_mol.setup_molecule(test_inp);
-    CHECK(test_mol.centers[0][0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
+    CHECK(test_mol.centers[0][1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
     CHECK(test_mol.charge == 0);
     CHECK(test_mol.multiplicity == 1);
     CHECK(test_mol.restricted == false);
     CHECK(test_mol.E_nuc == doctest::Approx(9.194863688325974).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
 
-    std::string xyz_ref = "3\nPOLYQUANT Dumped XYZ centers = all\nH    0.756968  "
-                          "  0.000000    -0.585875\nH    -0.756968    0.000000   "
-                          " -0.585875\nO    0.000000    0.000000    0.000000";
+    std::string xyz_ref = "3\nPOLYQUANT Dumped XYZ centers = all\nH    0.000000    0.756968    -0.516949\nH    -0.000000    -0.756968    -0.516949\nO    0.000000    0.000000    0.068926";
     std::string xyz_test = test_mol.dump_xyz();
     CHECK(xyz_test == xyz_ref);
   }
   TEST_CASE("MOLECULE: Construct Molecule with no keywords") {
     POLYQUANT_INPUT test_inp("../../tests/data/h2o_sto3glibrary/h2o_nokeyword.json");
     POLYQUANT_MOLECULE test_mol(test_inp);
-    CHECK(test_mol.centers[0][0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
+    CHECK(test_mol.centers[0][1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
     CHECK(test_mol.charge == 0);
     CHECK(test_mol.multiplicity == 1);
     CHECK(test_mol.restricted == true);
@@ -33,7 +31,7 @@ TEST_SUITE("MOLECULE") {
   TEST_CASE("MOLECULE: Construct Molecule with quantum nuclei specificed by center list.") {
     POLYQUANT_INPUT test_inp("../../tests/data/h2o_sto3g_quantumHlibrary/h2o.json");
     POLYQUANT_MOLECULE test_mol(test_inp);
-    CHECK(test_mol.centers[1][0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
+    CHECK(test_mol.centers[1][1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
     CHECK(test_mol.charge == 0);
     CHECK(test_mol.multiplicity == 1);
     CHECK(test_mol.restricted == false);
@@ -42,7 +40,7 @@ TEST_SUITE("MOLECULE") {
             "center label.") {
     POLYQUANT_INPUT test_inp("../../tests/data/h2o_sto3g_quantumHlibrary/h2o_alternate.json");
     POLYQUANT_MOLECULE test_mol(test_inp);
-    CHECK(test_mol.centers[1][0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
+    CHECK(test_mol.centers[1][1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_TIGHT));
     CHECK(test_mol.charge == 0);
     CHECK(test_mol.multiplicity == 1);
     CHECK(test_mol.restricted == false);
