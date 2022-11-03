552ca9652da1aa2b5df3ba0bf0b3903761879431
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-11-03 16:07:24 -0400

    Fix tests

--- tests/unit_tests/basis/basis_test.cpp
+++ tests/unit_tests/basis/basis_test.cpp
@@ -11,13 +11,13 @@ TEST_SUITE("BASIS") {
     POLYQUANT_BASIS test_bas;
     test_bas.load_basis(test_inp, test_mol);
     CHECK(test_bas.num_basis[0] == 7);
-    CHECK(test_bas.basis[0][0].O[0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_LOOSE));
+    CHECK(test_bas.basis[0][0].O[1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_LOOSE));
   }
   TEST_CASE("BASIS: Set up basis from file.") {
     POLYQUANT_INPUT test_inp("../../tests/data/h2o_sto3gfile/h2o.json");
     POLYQUANT_MOLECULE test_mol(test_inp);
     POLYQUANT_BASIS test_bas(test_inp, test_mol);
     CHECK(test_bas.num_basis[0] == 7);
-    CHECK(test_bas.basis[0][0].O[0] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_LOOSE));
+    CHECK(test_bas.basis[0][0].O[1] == doctest::Approx(1.4304631499).epsilon(POLYQUANT_TEST_EPSILON_LOOSE));
   }
 }
