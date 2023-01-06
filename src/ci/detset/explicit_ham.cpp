d0b3511567132dacbb71bee55565d6625d30a48c
Shiv Upadhyay <shivnupadhyay@gmail.com>
2023-01-05 19:27:39 -0500

    screening for explicit ham determinant set

--- src/ci/detset/explicit_ham.cpp
+++ src/ci/detset/explicit_ham.cpp
@@ -72,6 +72,12 @@ template <typename T> void POLYQUANT_DETSET<T>::create_ham() {
     APP_ABORT(ss.str());
   }
   this->ham.makeCompressed();
+  std::stringstream ss;
+  ss << "Created hamiltonian matrix." << std::endl;
+  ss << "  number of nonzero matrix elem before pruning: " << this->ham.nonZeros() << std::endl;
+  this->ham.prune(this->screening_threshold, 1.0);
+  ss << "  number of nonzero matrix elem after  pruning: " << this->ham.nonZeros() << std::endl;
+  Polyquant_cout(ss.str());
   // Polyquant_dump_sparse_mat_to_file(ham, "ci_ham.txt");
 }
 
