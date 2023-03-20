e70428c6249e52f65bce955b0eeed62da5e7fbb7
Shiv Upadhyay <shivnupadhyay@gmail.com>
2023-03-20 11:33:35 -0400

    Set pg from input

--- src/molecule/molecule.cpp
+++ src/molecule/molecule.cpp
@@ -91,13 +91,23 @@ void POLYQUANT_MOLECULE::symmetrize_molecule() {
       //   APP_ABORT("Symmetry handler point group is already set, but this calculation is not an atom. Currently the point group can only be manually specified for atoms as SO(3).");
       // }
 
+      if (input_symm->point_group.empty()) {
       if (MSYM_SUCCESS != (ret = msymFindSymmetry(ctx))) {
-
         auto error = msymErrorString(ret);
         std::cout << error << std::endl;
         error = msymGetErrorDetails();
         std::cout << error << std::endl;
         APP_ABORT("Error Figuring out symmetry");
+      }
+      } else {
+          std::string pg = this->input_symm->point_group;
+        if (MSYM_SUCCESS != (ret = msymSetPointGroupByName(ctx, pg.c_str()))) {
+          auto error = msymErrorString(ret);
+          std::cout << error << std::endl;
+          error = msymGetErrorDetails();
+          std::cout << error << std::endl;
+          APP_ABORT("Error setting PG to custom PG");
+        }
       }
     } else {
       // single atom
